8e1d62aba70dd53bd2f1c5df9dd7fb22948461b5
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2024-10-25 11:06:14 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import Contact from './Components/Contact';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct imports
-// import DiaryHolder from './Components/DiaryHolder';
 import Hero from './Components/Hero';
 
 function App() {
   return (
     <>
-      <Router> {/* Wrap everything in Router */}
-        
-        <Routes>
-          <Route path='/' element={<Hero />} />
-          {/* <Route path='/Diary' element={<DiaryHolder />} /> */}
-        </Routes>
-      </Router>
+    <Hero />
       <footer style={{ background: 'white' }}>
         <Contact />
         <div style={{ display: 'flex', zIndex: 50, justifyContent: 'center', paddingBottom: '10px' }}>
